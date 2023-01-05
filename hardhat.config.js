78870fbd6a6169e33988347b9ede4a506016921c
PanoptisDev <87525579+panoptisDev@users.noreply.github.com>
2023-01-05 02:16:02 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
+const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY;
+const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY;
 
 const fs = require('fs')
-const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789"
+// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789"
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -10,9 +14,15 @@ module.exports = {
       chainId: 1337
     },
     mumbai: {
-      url: "https://rpc-mumbai.matic.today",
-      accounts: [privateKey]
+      url: "https://rpc.ankr.com/polygon_mumbai",
+      accounts: [MUMBAI_PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: POLYGONSCAN_KEY,
+    },
+
   },
   solidity: {
     version: "0.8.4",
